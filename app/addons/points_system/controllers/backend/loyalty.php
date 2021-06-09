8ea29095825c37b7097402faa3fde841118cdebd
BadGomez <valtersimoesgamer@hotmail.com>
2021-06-09 14:35:23 +0100

    Alterar nome das chains

--- app/addons/points_system/controllers/backend/loyalty.php
+++ app/addons/points_system/controllers/backend/loyalty.php
@@ -6,18 +6,18 @@ if(!defined('BOOTSTRAP')) {die('Access denied');}
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     if($mode == 'manage'){
         //ir a BD buscar os dados de utilizador
-        $chain = "Select distinct(fk_user_id) id_user, sum(pontos) pontos_ganhos, firstname, lastname from cscart_points_system as P Inner Join cscart_users AS U on P.fk_user_id = U.user_id group by user_id";
+        $chainTotal = "Select distinct(fk_user_id) id_user, sum(pontos) pontos_ganhos, firstname, lastname from cscart_points_system as P Inner Join cscart_users AS U on P.fk_user_id = U.user_id group by user_id";
 
-        $points_list = db_get_array($chain);
+        $points_list = db_get_array($chainTotal);
 
         Tygh::$app['view'] -> assign('dados', $points_list);  
     }
     
     if($mode == 'update'){
         $id_passado = $_GET['id_user']; 
-        $chain = "Select firstname, lastname, pontos, data from cscart_points_system as P Inner Join cscart_users AS U on P.fk_user_id = U.user_id where user_id='$id_passado'";
+        $chainPessoa = "Select firstname, lastname, pontos, data from cscart_points_system as P Inner Join cscart_users AS U on P.fk_user_id = U.user_id where user_id='$id_passado'";
         
-        $points_pessoa_list = db_get_array($chain);
+        $points_pessoa_list = db_get_array($chainPessoa);
 
         Tygh::$app['view'] -> assign('dados', $points_pessoa_list);
     }
@@ -28,17 +28,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $quantity = $_POST['quantityPoints'];
         $id_adicionar = $_POST['idescondido'];
 
-        $chain = "Insert into cscart_points_system(fk_user_id, pontos) values ('$id_adicionar', '$quantity')";
+        $chainAtribuirPontos = "Insert into cscart_points_system(fk_user_id, pontos) values ('$id_adicionar', '$quantity')";
         
         if (empty($_POST['quantityPoints']) || $quantity == 0){
             echo "Invalid Value";
         }else{
-            db_query($chain);
+            db_query($chainAtribuirPontos);
         }
 
-        $chain2 = "Select distinct(fk_user_id) id_user, sum(pontos) pontos_ganhos, firstname, lastname from cscart_points_system as P Inner Join cscart_users AS U on P.fk_user_id = U.user_id group by user_id";
+        $chainTotal2 = "Select distinct(fk_user_id) id_user, sum(pontos) pontos_ganhos, firstname, lastname from cscart_points_system as P Inner Join cscart_users AS U on P.fk_user_id = U.user_id group by user_id";
 
-        $points_list = db_get_array($chain2);
+        $points_list = db_get_array($chainTotal2);
 
         Tygh::$app['view'] -> assign('dados', $points_list); 
     }
