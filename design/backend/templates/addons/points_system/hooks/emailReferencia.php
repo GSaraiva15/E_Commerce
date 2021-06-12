925f5cb17061ac96d9e3a42f4980649ef4554534
GSaraiva15 <goncalosaraiva00@gmail.com>
2021-06-12 15:55:35 +0100

    Passar id email Referencia

--- design/backend/templates/addons/points_system/hooks/emailReferencia.php
+++ design/backend/templates/addons/points_system/hooks/emailReferencia.php
@@ -1,6 +1,6 @@
 <?php
 
-include('hooks.php');
+include('/design/backend/templates/addons/points_system/hooks.php');
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     if($mode == 'profile_fields'){
@@ -8,7 +8,10 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
         $chainEmailRef = "Select user_id, email from cscart_users where email = '$emailReferencia'";
     
         $emailReferencia = $_POST['elm_emailReferencia'];
+        
+        get_record($chainEmailRef);
 
+        give_points_register($emailReferencia.user_id);
         
     }
 }
